36723f96a9b7a5d52ba8556e730aa39248567cc2
Divya Gupta <divygupta0319@gmail.com>
2023-02-16 22:14:01 +0530

    When multiple implementations for injecting dependency available, using @Qualifier annotation to tell which implementation to use

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -1,6 +1,7 @@
 package com.luv2code.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,6 +9,7 @@ public class TennisCoach implements Coach {
 	
 	
 	@Autowired
+	@Qualifier("happyFortuneService")
 	private FortuneService fortuneService;
 	
 	// define a default constructor
