f772599af1d913eb08d9500d4af1b0a1098fc9b5
Divya Gupta <divygupta0319@gmail.com>
2023-02-16 18:53:37 +0530

    Field injection using Autowired annotation

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 	
+	
+	@Autowired
 	private FortuneService fortuneService;
 	
 	// define a default constructor
@@ -14,11 +16,13 @@ public class TennisCoach implements Coach {
 	}
 	
 	// define a setter method
+	/*
 	@Autowired
 	public void doSomeCrazyStuff(FortuneService fortuneService) {
 		System.out.println("TennisCoach: inside doSomeCrazyStuff method");
 		this.fortuneService = fortuneService;
 	}
+	*/
 	
 	/*
 	@Autowired
