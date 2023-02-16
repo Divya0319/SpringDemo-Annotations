538dcf339abcdf7f9c2285195f07c151e2a55340
Divya Gupta <divygupta0319@gmail.com>
2023-02-16 17:09:24 +0530

    Default component names - Spring annotations

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -2,7 +2,7 @@ package com.luv2code.springdemo;
 
 import org.springframework.stereotype.Component;
 
-@Component("ourTennisCoach")
+@Component
 public class TennisCoach implements Coach {
 
 	@Override
